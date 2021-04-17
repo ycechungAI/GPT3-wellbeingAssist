5379a8f8b02ce54ee68105c19f3e71e23eedf608
Jordi Spranger <jordispranger@aol.com>
2021-04-17 15:14:16 +0200

    added the tamplate for function definitions

---
+++ backend_functions.py
@@ -0,0 +1,32 @@
+#Did it fulfill our question? [True, False]
+def patient_answered_question(text)
+    #GPT-3 magic
+    if True:
+        return True
+    elif False:
+        return False
+
+#Do we need to ask a follow-up question? [True, False]
+def need_to_ask_followup(text):
+    #GPT-3 magic
+    if True:
+        return True
+    elif False:
+        return False
+
+#What do we need to ask more specifically about? [Next Question For Patient]
+def what_to_ask_next(text):
+    #GPT-3 magic
+    question = "GPT-3's next question"
+    return question
+
+#What are his symptoms and when did they occur? [List of Symptoms]
+def extract_symptoms_from_patient_answer(Text):
+    #GPT-3 magic
+    list_of_symptoms = ["coughing", "pain in chest"]
+    return list_of_symptoms
+
+
+#Save new entry to database ? [Save to database]
+def save_to_database(new_row):
+    #Databse Magic
