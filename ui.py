9a4f26d7949009452c9413924a9ce6ac470cf1a2
Jordi Spranger <jordispranger@aol.com>
2021-04-18 14:50:49 +0200

    UI is getting an update

--- ui.py
+++ ui.py
@@ -4,6 +4,7 @@ import time
 from fpdf import FPDF
 import base64
 
+r = ""
 
 # Formatting the app bg and text color
 st.markdown(
@@ -17,15 +18,12 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-# interact with FastAPI endpoint
-backend = 'http://fastapi:8000/code'
+
 
 def process_prompt(input, server_url: str):
-    r = requests.post(server_url,
-                      json={"input": input},
-                      timeout=8000)
+    r += '\n\n'+str(input.text)
 
-    return r.text
+    return r
 
 
 # Setting up the Title
@@ -37,10 +35,25 @@ st.write('''Check in with me regularly to improve drug research.''')
 
 st.image('./ai-bot.jpg', use_column_width=True)
 
+st.write(r)
 input = st.text_input('Send Robo a message:')
 
 if st.button('Send'):
-    st.subheader('Send')
     with st.spinner(text='Sending Message...'):
-        report_text = process_prompt(input, backend)
-        st.code(report_text)
+        f = open("storage.txt", "a")
+        f.write(str(input + '  \n'))
+        f.close()
+
+        #f = open("storage.txt", "r")
+        #r = f.readlines()
+        r = reversed(list(open("storage.txt", "r").readlines()))
+        text = ""
+        for line in r:
+            text += line + '  \n'
+        st.text(text)
+
+
+
+        #r = process_prompt(input)
+        #st.write(r)
+        #st.code(report_text)
